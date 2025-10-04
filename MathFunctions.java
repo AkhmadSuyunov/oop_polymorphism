7be530248832f5c3c809250caf4da2af9b5861f0
ahmadjon.suyunov <akhmadsuyunov95@gmail.com>
2025-10-04 11:51:50 +0500

    polymorphism MathFunctions task

---
+++ MathFunctions.java
@@ -0,0 +1,86 @@
+package polymorphism;
+
+public class MathFunctions {
+    public int add(int a, int b){
+        return a+b;
+    }
+    public float add(float a, float b){
+        return a+b;
+    }
+    public double add(double a, double b){
+        return a+b;
+    }
+    public long add(long a, long b){
+        return a+b;
+    }
+
+
+    public int sub(int a, int b){
+        return a-b;
+    }
+    public float sub(float a, float b){
+        return a-b;
+    }
+    public double sub(double a, double b){
+        return a-b;
+    }
+    public long sub(long a, long b){
+        return a-b;
+    }
+
+
+    public int multiply(int a, int b){
+        return a*b;
+    }
+    public float multiply(float a, float b){
+        return a*b;
+    }
+    public double multiply(double a, double b){
+        return a*b;
+    }
+    public long multiply(long a, long b){
+        return a*b;
+    }
+
+
+    public int div(int a, int b){
+        return a/b;
+    }
+    public float div(float a, float b){
+        return a/b;
+    }
+    public double div(double a, double b){
+        return a/b;
+    }
+    public long div(long a, long b){
+        return a/b;
+    }
+
+
+    public int abs_method(int a){
+        return a < 0 ? -a : a;
+    }
+    public float abs_method(float a){
+        return a < 0 ? -a : a;
+    }
+    public double abs_method(double a){
+        return a < 0 ? -a : a;
+    }
+    public long abs_method(long a){
+        return a < 0 ? -a : a;
+    }
+
+
+    public int pow_method(int a){
+        return a*a;
+    }
+    public float pow_method(float a){
+        return a*a;
+    }
+    public double pow_method(double a){
+        return a*a;
+    }
+    public long pow_method(long a){
+        return a*a;
+    }
+}
